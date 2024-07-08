427472cafc1bc42a2236cb2ea7aa5f8655ca1894
Kurt De Neve <kdeneve@outlook.be>
2024-07-08 07:25:20 +0200

    Update 05-closures.js

--- oefening1/js/05-closures.js
+++ oefening1/js/05-closures.js
@@ -5,8 +5,20 @@
 // De functie makeStringRepeater retourneert een functie. Deze functie heeft 1 parameter,
 // i.e. een string, en retourneert de string, nrOfTimes aan elkaar geplakt
 
+const makeStringRepeater = function (nrOfTimes) {
+  const repeater = function (woord) {
+    let zin = "";
+    for (let x = 0; x < nrOfTimes; x++) {
+      zin += woord;
+    }
+    return zin;
+  };
+
+  return repeater;
+};
+
 // Gebruik dit om je code te testen:
-const stringDoubler = makeStringRepeater(2);
-const stringTripler = makeStringRepeater(3);
+const stringDoubler = makeStringRepeater(6);
+const stringTripler = makeStringRepeater(12);
 console.log(stringDoubler('abc ')); // abc abc
 console.log(stringTripler('def ')); // def def def
